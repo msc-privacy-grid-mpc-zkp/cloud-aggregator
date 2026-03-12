32f0a15efb5b0eefd8dbdba05eaeb2c259edda13
Uslje <147374020+uslje13@users.noreply.github.com>
2026-03-12 18:56:46 +0100

    chore: remove keys directory from version control

--- cmd/aggregator/main.go
+++ cmd/aggregator/main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+
 	"github.com/msc-privacy-grid-mpc-zkp/cloud-aggregator/internal/api"
 	"github.com/msc-privacy-grid-mpc-zkp/cloud-aggregator/internal/zkp"
-	"log"
 )
 
 func main() {
-	fmt.Println("☁️  Starting MPC Cloud Aggregator...")
+	// 1. Definišemo parametre (zastavice) koje možemo proslediti iz terminala
+	port := flag.String("port", "8080", "Port na kom server sluša")
+	name := flag.String("name", "Server A", "Ime MPC čvora")
+	flag.Parse()
 
-	// 1. Učitavamo ključ sa diska
-	// (Pretpostavka je da si 'verifying.key' iskopirao u root projekta ili u folder 'keys')
+	fmt.Printf("☁️  Starting MPC Cloud Aggregator: [%s]\n", *name)
+	fmt.Printf("---------------------------------------------------------\n")
+
+	// 2. Učitavamo ključ sa diska
 	vk, err := zkp.LoadVerifyingKey("keys/verifying.key")
 	if err != nil {
 		log.Fatalf("[FATAL] Failed to load verifying key: %v", err)
 	}
 	fmt.Println("[SECURITY] ZKP Verifying Key loaded successfully!")
 
-	// 2. Startujemo server i prosleđujemo mu ključ
+	// 3. Pravimo memoriju koja čeka 10 brojila
 	store := api.NewMemoryStore(10)
-	api.StartServer(":8080", vk, store)
+
+	// 4. Startujemo server, ali sada koristimo dinamički port!
+	address := ":" + *port
+	api.StartServer(address, vk, store)
 }
